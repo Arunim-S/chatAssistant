dc8033a65bed3e4ad5d5add10c591ce858d71037
Arunim-S <arunims@motaai.dev>
2024-01-30 11:47:43 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hi</h1> 
+        <p>How may I help you ?</p>
+        <input placeholder='search anything here ...' className='search'></input>
+      </div>
+    </>
+  )
+}
+
+export default App
